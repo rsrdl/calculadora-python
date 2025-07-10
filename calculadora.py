56e3b74e86f827c626142655f9562405fe3ed11f
José Olivar de Lima <olivanlima@gmail.com>
2025-07-10 19:36:52 -0300

    Primeiro commit

---
+++ calculadora.py
@@ -0,0 +1,22 @@
+# Calculadora
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+operacao = input('Digitea operação desejada (+, -, *, /): ')
+
+match operacao:
+    case '+':
+        resp = num1 + num2
+    case '-':
+        resp = num1 - num2
+    case '*':
+        resp = num1 * num2
+    case '/':
+        if num2 == 0:
+            resp = "Erro: Divisão por zero não é permitida."
+        else:
+            resp = num1 / num2
+    case _:
+        resp = "Operação inválida."
+        
+print(resp)
